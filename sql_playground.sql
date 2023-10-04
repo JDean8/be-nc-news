548adce05bf313aefac3dc9dbde3b6dac2dcd1ff
JDean8 <jdean8@live.co.uk>
2023-10-04 11:42:08 +0100

    Updating .gitignore and merging main

--- sql_playground.sql
+++ sql_playground.sql
@@ -9,24 +9,9 @@
 -- SELECT * FROM comments;
 
 SELECT
-articles.article_id,
-article_img_url,
-articles.author,
-articles.body,
-articles.created_at,
-title,
-topic,
-articles.votes,
+articles.article_id, article_img_url, articles.author, articles.body, articles.created_at, title, topic, articles.votes,
 COUNT(comment_id)::INT AS comment_count
-FROM articles
-JOIN comments ON articles.article_id = comments.article_id
+FROM articles JOIN comments ON articles.article_id = comments.article_id
 GROUP BY
-articles.article_id,
-article_img_url,
-articles.author,
-articles.body,
-articles.created_at,
-title,
-topic,
-articles.votes
+articles.article_id, article_img_url, articles.author, articles.body, articles.created_at, title, topic, articles.votes
 HAVING articles.article_id = 1;
